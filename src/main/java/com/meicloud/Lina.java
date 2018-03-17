0535f08592424351318ee3cdf85a8cf9d8aded13
Yungkid <910321445@qq.com>
2018-03-17 09:12:11 +0800

    add Lina.java

---
+++ src/main/java/com/meicloud/Lina.java
@@ -0,0 +1,12 @@
+package com.meicloud;
+
+public class Lina extends Gina {
+    @Override
+    public void sayHi(String name) {
+        super.sayHi(name);
+    }
+
+    public void eat(String foodName){
+        System.out.println("I eat "+foodName);
+    }
+}
